6216b998f13e804719547fb84ba4261ba7d6fd4a
Piton <brava_05@mail.ru>
2022-06-21 10:44:52 +0300

    My commit 2

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -11,7 +11,7 @@ from django.test import Client, TestCase, override_settings
 
 
 User = get_user_model()
-TEMP_MEDIA_ROOT = tempfile.mkdtemp(dir=settings.MEDIA_ROOT)
+TEMP_MEDIA_ROOT = tempfile.mkdtemp(dir=settings.BASE_DIR)
 
 
 @override_settings(MEDIA_ROOT=TEMP_MEDIA_ROOT)
